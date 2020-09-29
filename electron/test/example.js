cdeddc357719088c4094052b27b1449453d8c656
Tao Ma <taoma2011@gmail.com>
2020-09-28 23:46:38 -0700

    start to add version graph api

--- electron/test/example.js
+++ electron/test/example.js
@@ -1,5 +1,7 @@
-describe('description', () => {
-  it('should have description', () => {
-    expect(1 + 2).toBe(3);
+var expect = require("chai").expect;
+
+describe("description", () => {
+  it("should have description", () => {
+    expect(1 + 2).to.equal(3);
   });
 });
