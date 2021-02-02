093416391ad58dbbee29f857a0af98672a039e17
Tao Ma <taoma2011@gmail.com>
2021-02-01 23:24:40 -0800

    tmp

--- electron/app/components/SearchControl.js
+++ electron/app/components/SearchControl.js
@@ -16,31 +16,7 @@ export const SearchControl = (props) => {
     setSearchText(event.target.value);
   };
 
-  const doSearch = () => {
-    console.log("doc = ", doc);
-    var maxPages = doc.numPages;
-    var countPromises = []; // collecting all page promises
-    for (var j = 1; j <= maxPages; j++) {
-      var page = doc.getPage(j);
-
-      var txt = "";
-      countPromises.push(
-        page.then(function(page) {
-          // add page promise
-          var textContent = page.getTextContent();
-          return textContent.then(function(text) {
-            // return content promise
-            console.log(text.items);
-            /*return text.items
-              .map(function(s) {
-                return s.str;
-              })
-              .join("");*/ // value page text
-          });
-        })
-      );
-    }
-  };
+  const doSearch = () => {};
   return (
     <Grid container spacing={2}>
       <Grid item>
