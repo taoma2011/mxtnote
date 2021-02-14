f9c51094bd80222edc2f95ce55bc3f9a7c33d8c7
Tao Ma <taoma2011@gmail.com>
2021-02-14 11:51:02 -0800

    fix split pane appearance

--- electron/src/components/FilePage.js
+++ electron/src/components/FilePage.js
@@ -1,33 +1,24 @@
 /* eslint-disable react/prop-types */
-import React, { useRef, useEffect } from "react";
-import { useSelector } from "react-redux";
-import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
-import FileControl from "../containers/FileControl";
-import { SearchControl } from "../components/SearchControl";
-import { SearchResult } from "../components/SearchResult";
-import DeleteNoteDialog from "../containers/DeleteNoteDialog";
-
-import { PageWrapper } from "../components/PageWrapper";
-import LoadNote from "../containers/LoadNote";
-import LoadFile from "../containers/LoadFile";
-import LoadSettings from "../containers/LoadSettings";
-//import LoadPage from "../containers/LoadPage";
-
-//import { updatePageScroll } from "../actions/file";
-
-import { FixedSizeList as List } from "react-window";
-
-//import Divider from "@material-ui/core/Divider";
-import SplitPane from "react-split-pane";
-import BackupDb from "../containers/BackupDb";
-import { selectSearchText } from "../features/search/searchSlice";
-
-//const { BrowserWindow } = require("electron").remote;
+import React, { useRef, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import { FixedSizeList as List } from 'react-window';
+import SplitPane from 'react-split-pane';
+import FileControl from '../containers/FileControl';
+import { SearchControl } from './SearchControl';
+import { SearchResult } from './SearchResult';
+import DeleteNoteDialog from '../containers/DeleteNoteDialog';
+import { PageWrapper } from './PageWrapper';
+import LoadNote from '../containers/LoadNote';
+import LoadFile from '../containers/LoadFile';
+import LoadSettings from '../containers/LoadSettings';
+import BackupDb from '../containers/BackupDb';
+import { selectSearchText } from '../features/search/searchSlice';
 
 export const FilePage = (props) => {
   // eslint-disable-next-line react/prop-types
-  //const file = useSelector((state) => state.file);
+  // const file = useSelector((state) => state.file);
   const {
     status,
     doc,
@@ -42,8 +33,6 @@ export const FilePage = (props) => {
     pageHeight,
     updatePageScroll,
   } = props;
-  //console.log("all notes ", notes);
-  //const dispatch = useDispatch();
 
   /*
   const pageDivStyle = {
@@ -70,18 +59,14 @@ export const FilePage = (props) => {
     );
   };
 
-  //console.log("browser window is ", BrowserWindow);
-  const displayPageHeight = React.useMemo(
-    () => (pageHeight ? pageHeight : 80),
-    [pageHeight]
-  );
-  console.log("display page height = ", displayPageHeight);
+  const displayPageHeight = React.useMemo(() => pageHeight || 80, [pageHeight]);
+  console.log('display page height = ', displayPageHeight);
 
   const scrollUpdate = ({ scrollOffset }) => {
     if (pageHeight) {
       const newPageNum = Math.floor((scrollOffset + 1) / pageHeight) + 1;
-      if (newPageNum != pageNum) {
-        console.log("update page scroll: ", newPageNum);
+      if (newPageNum !== pageNum) {
+        console.log('update page scroll: ', newPageNum);
         updatePageScroll(newPageNum);
       }
     }
@@ -93,20 +78,20 @@ export const FilePage = (props) => {
   const listRef = useRef(null);
   useEffect(() => {
     if (listRef && listRef.current && pageHeight) {
-      console.log("scroll in useEffect");
+      console.log('scroll in useEffect');
       listRef.current.scrollToItem(pageNum - 1);
     }
   });
   const pages = () => {
-    if (status === "ready") {
-      console.log("doc is ready, displayPageHeight is ", displayPageHeight);
-      console.log("num page is ", numPages);
+    if (status === 'ready') {
+      console.log('doc is ready, displayPageHeight is ', displayPageHeight);
+      console.log('num page is ', numPages);
 
       let initialScrollOffset = 0;
       // if we have page height, we scroll to the desired page
       if (pageHeight) {
         initialScrollOffset = (pageNum - 1) * pageHeight + 1;
-        console.log("set initial scroll to ", initialScrollOffset);
+        console.log('set initial scroll to ', initialScrollOffset);
 
         const list = (
           <List
@@ -122,33 +107,30 @@ export const FilePage = (props) => {
           </List>
         );
         return list;
-      } else {
-        console.log("render only the first page");
-        return (
-          <PageWrapper
-            pageNum={1}
-            notes={notes}
-            pdfDoc={doc}
-            key="test-page"
-            fileId={fileId}
-            pageWidth={0}
-            pageHeight={0}
-          />
-        );
       }
-    } else {
-      return <p>loading file</p>;
+      console.log('render only the first page');
+      return (
+        <PageWrapper
+          pageNum={1}
+          notes={notes}
+          pdfDoc={doc}
+          key="test-page"
+          fileId={fileId}
+          pageWidth={0}
+          pageHeight={0}
+        />
+      );
     }
+    return <p>loading file</p>;
   };
 
   const viewPortHeight = displayPageHeight;
 
   const styles = {
-    background: "#000",
-    width: "2px",
-    cursor: "col-resize",
-    margin: "0 5px",
-    height: "100%",
+    background: 'blue',
+    width: '5px',
+    cursor: 'col-resize',
+    height: '100%',
   };
 
   const searchText = useSelector(selectSearchText);
@@ -156,8 +138,8 @@ export const FilePage = (props) => {
     <Box display="flex" flexDirection="column">
       <div
         style={{
-          background: "white",
-          width: "100%",
+          background: 'white',
+          width: '100%',
         }}
       >
         <Box display="flex" flexDirection="row">
@@ -166,7 +148,7 @@ export const FilePage = (props) => {
         </Box>
       </div>
       <div style={{ height: viewPortHeight }}>
-        <Paper height="100%" style={{ height: "100%" }}>
+        <Paper height="100%" style={{ height: '100%' }}>
           {!noteLoaded && <LoadNote />}
           {!settingsLoaded && <LoadSettings />}
           {docLoading && <LoadFile />}
@@ -176,7 +158,7 @@ export const FilePage = (props) => {
             <SplitPane
               split="vertical"
               minSize={200}
-              style={{ position: "relative" }}
+              style={{ position: 'relative' }}
               resizerStyle={styles}
             >
               <div>
@@ -193,3 +175,5 @@ export const FilePage = (props) => {
     </Box>
   );
 };
+
+export default FilePage;
