6b73e78e368d14c1a66844d899074e6b61cdb09e
Tao Ma <taoma2011@gmail.com>
2021-03-13 01:00:18 -0800

    add missing file

---
+++ electron/mxtnote.js
@@ -0,0 +1,5 @@
+import Root from './src/containers/Root';
+import { configureStore } from './src/store/configureStore';
+import { InitDb } from './src/utils/db';
+
+export { Root, configureStore, InitDb };
